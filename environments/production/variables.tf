f06d82fdf75b08551dcc48fcde6ce0a6b3349a6d
Mr-Maqsood <mk253751@gmail.com>
2026-03-22 13:39:09 +0000

    files added

---
+++ environments/production/variables.tf
@@ -0,0 +1,18 @@
+
+variable "bucket_name" {
+    type = string
+    
+
+  
+}
+variable "bucket_tags" {
+    type = map(string)
+
+    
+
+  
+}
+variable "version_status" {
+    type = string
+    
+}
